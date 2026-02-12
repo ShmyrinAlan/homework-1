5b87ca5bdef753b49dcfb35b9a15d86acf787955
Alan <alanshmyrinsource@gmail.com>
2026-02-12 22:27:29 +0500

    fix realization for warrior ability

--- src/main/java/com/narxoz/rpg/character/Warrior.java
+++ src/main/java/com/narxoz/rpg/character/Warrior.java
@@ -3,6 +3,10 @@ package com.narxoz.rpg.character;
 import lombok.Data;
 
 import java.util.Random;
+import java.util.concurrent.*;
+import java.util.concurrent.atomic.AtomicBoolean;
+import java.util.concurrent.atomic.AtomicInteger;
+import java.util.concurrent.atomic.AtomicReference;
 
 /**
  * Example concrete implementation of a Character.
@@ -20,38 +24,29 @@ import java.util.Random;
  * - Archer (balanced stats, ranged combat)
  * - (Optional) Additional classes: Rogue, Paladin, etc.
  */
-@Data
 public class Warrior implements Character {
 
-    private String name;
-    private int health;
-    private int mana;
-    private int strength;
-    private int intelligence;
-    private Random rn;
-    private Thread ability;
+    private final AtomicReference<String> name;
+    private final AtomicInteger health;
+    private final AtomicInteger mana;
+    private final AtomicInteger strength;
+    private final AtomicInteger intelligence;
+    private final AtomicBoolean isBerserk;
+    private final ScheduledExecutorService service;
 
     // TODO: Add fields for equipped weapon and armor
     // Think: Should Warrior know about its equipment?
 
     public Warrior(String name) {
-        this.name = name;
-        this.rn = new Random();
+        this.name = new AtomicReference<>(name);
+        Random rn = new Random();
         // Warrior stats: high health and strength, low mana and intelligence
-        this.health = rn.nextInt(150, 200);
-        this.mana = rn.nextInt(10,50);
-        this.strength = rn.nextInt(70,100);
-        this.intelligence = rn.nextInt(5,23);
-        this.ability = new Thread(()->{
-            long endTime = System.nanoTime() + 10 *1_000_000_000L;
-            strength = (int)Math.round(strength*1.2);
-            health = (int)Math.round(health*1.3);
-            System.out.println("For the next 10s warrior gets a berserk mod(strength bonus 20% and health bonus 30%)");
-            while (System.nanoTime()<endTime){}
-            strength = (int)Math.round(strength/1.2);
-            health = (int)Math.round(health/1.3);
-            System.out.println("Berserk mod was ended");
-        });
+        this.health = new AtomicInteger(rn.nextInt(150, 200));
+        this.mana = new AtomicInteger(rn.nextInt(10,50));
+        this.strength = new AtomicInteger(rn.nextInt(70,100));
+        this.intelligence = new AtomicInteger(rn.nextInt(5,23));
+        this.isBerserk = new AtomicBoolean(false);
+        this.service = Executors.newScheduledThreadPool(1);
     }
 
     @Override
@@ -64,20 +59,54 @@ public class Warrior implements Character {
                 Mana: %d
                 Strength: %d
                 Intelligence: %d
-                """, name,health,mana,strength,intelligence);
+                """, name.get(),health.get(),mana.get(),strength.get(),intelligence.get());
     }
 
     @Override
     public void useSpecialAbility() {
-        if(ability.isAlive()){
+        if(isBerserk.get()){
             System.out.println("Ability is");
         }else {
-            ability.start();
+            strength.set((int)Math.round(strength.get()*1.2));
+            health.set((int)Math.round(health.get()*1.3));
+            isBerserk.set(true);
+            System.out.println("For the next 10s warrior gets a berserk mod(strength bonus 20% and health bonus 30%)");
+            service.schedule(() -> {
+                strength.set((int)Math.round(strength.get()/1.2));
+                health.set((int)Math.round(health.get()/1.3));
+                isBerserk.set(false);
+                System.out.println("Berserk mod was ended");
+            }, 10, TimeUnit.SECONDS);
         }
     }
 
     // TODO: Implement methods from Character interface
     // You need to define those methods in Character interface first!
+
+    @Override
+    public String getName() {
+        return name.get();
+    }
+
+    @Override
+    public int getHealth() {
+        return health.get();
+    }
+
+    @Override
+    public int getMana() {
+        return mana.get();
+    }
+
+    @Override
+    public int getStrength() {
+        return strength.get();
+    }
+
+    @Override
+    public int getIntelligence() {
+        return intelligence.get();
+    }
 
 
     // TODO: Add equipment-related methods
